9969bd4e40efc4961f2c6d77a84368e634429b20
shubhamtaneja <shubhamtaneja@uniqus.com>
2025-02-02 17:39:50 +0530

    override existing environment variables when loading .env file in main application

--- api/main.py
+++ api/main.py
@@ -1,5 +1,13 @@
 from dotenv import load_dotenv
-load_dotenv()
+
+# Force override any existing environment variables:
+load_dotenv(dotenv_path=r"C:\Users\ShubhamTaneja\backendModels\AIVisionBackend\.env", override=True)
+
+
+import os
+print("GOOGLE_APPLICATION_CREDENTIALS:",
+      os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
+
 
 from fastapi import FastAPI
 from .src.routes.users import user_router
